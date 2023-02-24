e1a440cc12309df6724e23beb72e3a11595ee868
Zach227 <zadriskill@gmail.com>
2023-02-24 11:45:22 -0700

    Added files for sim notebook

---
+++ Tutorials/using_simulation_tools/files/import_source.py
@@ -0,0 +1,27 @@
+import os
+import requests
+
+
+def cd(folder):
+    """My wrapper for os.chdir() so I can print it desired."""
+    if folder:
+        os.chdir(folder)
+
+
+def import_text(text):
+    url = (
+        "https://raw.githubusercontent.com/westonMS/tempColab/master/Tutorials/using_simulation_tools/files/%s"
+        % text
+    )
+    resp = requests.get(url)
+    with open(text, "wb") as f:
+        f.write(resp.content)
+
+
+def import_source():
+    cd("/content/tmp_code")
+    import_text("simTemplate.txt")
+    import_text("simTemplate2.txt")
+    import_text("bitwiseAnd.sv")
+    import_text("bitwiseAnd.stm")
+    cd("/content")
