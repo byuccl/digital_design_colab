a1024681709641b7d7fa94742c4c135970f8af0f
Weston Smith <wms29@byu.edu>
2023-02-24 13:34:06 -0700

    Added template folder

--- Templates/frqTemplate.py
+++ Templates/frqTemplate.py
@@ -83,57 +83,16 @@ def create_frq_string(list, index, q_width=500):
 """
 The dictionary that holds the questions and the answers.
 Answers can be String or Int type. This does change the function used to create the grid object as shown
+The key is Q + Question Index. 
 """
-frquestions = {
-    "Q0": ["Convert this binary number to decimal: 10110", 22],
-    "Q1": ["Convert this binary number to decimal: 10001", 17],
-    "Q2": ["Convert this binary number to decimal: 101011", 43],
-    "Q3": ["Convert this decimal number to binary: 9", 1001],
-    "Q4": ["Convert this decimal number to binary: 21", 10101],
-    "Q5": ["Convert this decimal number to binary: 63", 111111],
-    "Q6": ["Convert this hexadecimal number to decimal: A3E", 2622],
-    "Q7": ["Convert this hexadecimal number to binary: 5B", 1011011],
-    "Q8": ["Convert this hexadecimal number to binary: C9", 11001001],
-    "Q9": ["Convert this decimal number to hexadecimal: 137", 89],
-    "Q10": ["Convert this binary number to hexadecimal: 10000011 ", 83],
-    "Q11": ["Convert this binary number to hexadecimal: 1011111011101111", "BEEF"],
-}
-frq_1 = create_frq_int(frquestions, 0, 400)
+frquestions = {"Q0": ["Example Question", "Answer"]}
 
-frq_2 = create_frq_int(frquestions, 1, 400)
-
-frq_3 = create_frq_int(frquestions, 2, 400)
-
-frq_4 = create_frq_int(frquestions, 3, 400)
-
-frq_5 = create_frq_int(frquestions, 4, 400)
-
-frq_6 = create_frq_int(frquestions, 5, 400)
-frq_7 = create_frq_int(frquestions, 6, 400)
-frq_8 = create_frq_int(frquestions, 7, 400)
-
-frq_9 = create_frq_int(frquestions, 8, 400)
-frq_10 = create_frq_int(frquestions, 9, 400)
-
-
-frq_11 = create_frq_int(frquestions, 10, 400)
-
-frq_12 = create_frq_string(frquestions, 11, 400)
+# Define your FRQs here
+frq_1 = create_frq_int(frquestions, 0)
 
 
 frqs = {
     "Q0": frq_1,
-    "Q1": frq_2,
-    "Q2": frq_3,
-    "Q3": frq_4,
-    "Q4": frq_5,
-    "Q5": frq_6,
-    "Q6": frq_7,
-    "Q7": frq_8,
-    "Q8": frq_9,
-    "Q9": frq_10,
-    "Q10": frq_11,
-    "Q11": frq_12,
 }
 
 
