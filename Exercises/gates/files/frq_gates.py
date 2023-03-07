9342e1829ba2db03ae65534fd7861c2c4ffcbde5
Zach227 <zadriskill@gmail.com>
2023-03-07 11:50:37 -0700

    updated gates

--- Exercises/gates/files/frq_gates.py
+++ Exercises/gates/files/frq_gates.py
@@ -88,6 +88,10 @@ The key is Q + Question Index.
 frquestions = {
     "Q1": ["What is the output if A=1, B=1, C=0", "1", "Question 1"],
     "Q2": ["What is the output if A=1, B=0, C=1", "0", "Question 2"],
+    "Q3": ["What is the output if A=0, B=0, C=0", "1", "Question 3"],
+    "Q4": ["What is the output if A=0, B=1, C=1", "0", "Question 4"],
+    "Q5": ["What is the output if A=0, B=0, C=0, D=1", "0", "Question 5"],
+    "Q6": ["What is the output if A=1, B=0, C=1, D=0", "1", "Question 6"],
 }
 
 
