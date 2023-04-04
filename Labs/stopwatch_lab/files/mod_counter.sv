39ff6f81aae57be03b6bb9bf6d0bffb12d81c122
Zach227 <zadriskill@gmail.com>
2023-04-04 11:09:29 -0600

    fix bad file

--- Labs/stopwatch_lab/files/mod_counter.sv
+++ Labs/stopwatch_lab/files/mod_counter.sv
@@ -1,5 +1,20 @@
-clk
-reset increment 
-//Write the rest yourself. 
-//Separate the values by a single space
-//Each line is a new clock cycle.
+module mod_counter #(parameter MOD_VALUE=10, WID=4) (
+    input wire logic clk, reset, increment,
+    output logic rollover,
+    output logic [WID-1:0] count
+    );
+    
+    always_ff @(posedge clk)
+        if (reset)
+            count <= 0;
+        else if (increment)
+        begin
+            if (count == MOD_VALUE-1)
+                count <= 0;
+            else
+                count <= count + 1;
+        end
+            
+    assign rollover = ((count == MOD_VALUE-1) && (increment == 1))?1'b1:1'b0;
+        
+endmodule
--- Labs/stopwatch_lab/files/stopwatch.sv
+++ Labs/stopwatch_lab/files/stopwatch.sv
@@ -1,11 +1,19 @@
-clk
-reset run
-0 1
-0 1
-0 1
-0 1
-0 1
-0 1
-0 1
-0 1
-0 1
+
+module stopwatch(
+    input wire logic clk, reset, run,
+    output logic [3:0] digit0, digit1, digit2, digit3
+    );
+    
+    //internal signals
+    logic [19:0] count;
+    logic [3:0] rollover;
+    logic pulse;
+    
+    //counter to roll over every .01 seconds and then for each digit
+    mod_counter #(1000000, 20) C4(.count(count), .rollover(pulse), .clk(clk), .reset(reset), .increment(run));
+    mod_counter C0(.count(digit0), .rollover(rollover[0]), .clk(clk), .reset(reset), .increment(pulse));
+    mod_counter C1(.count(digit1), .rollover(rollover[1]), .clk(clk), .reset(reset), .increment(rollover[0]));
+    mod_counter C2(.count(digit2), .rollover(rollover[2]), .clk(clk), .reset(reset), .increment(rollover[1]));
+    mod_counter #(6, 4) C3(.count(digit3), .rollover(rollover[3]), .clk(clk), .reset(reset), .increment(rollover[2]));
+    
+endmodule
