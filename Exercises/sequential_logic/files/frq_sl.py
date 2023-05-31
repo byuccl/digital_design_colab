987507a8a8ee12667c4d1b31100a080a036466c5
Adam Shaeffer <49406062+adamshaeffer@users.noreply.github.com>
2023-05-31 14:32:29 -0600

    Update frq_sl.py

--- Exercises/sequential_logic/files/frq_sl.py
+++ Exercises/sequential_logic/files/frq_sl.py
@@ -91,7 +91,7 @@ frquestions = {
     "Q3": ["In ns:", "0.1", "If the frequency is 10 GHZ what is the period?"],
     "Q4": ["In ms:", 40, "If the frequency is 25 Hz what is the period?"],
     "Q5": ["In ns", 10, "If the frequency of the Clock is 100 MHZ, what is the period?"],
-    "Q6": ["Number of clock cycles:", 10000000, "Using the same clock frequency as the last question, how many clock cycles occur in 1/10th of a second?"],
+    "Q6": ["Number of clock cycles:", 10000000, "Using the same clock from Q5, how many clock cycles occur in 1/10th of a second?"],
 }
 
 
