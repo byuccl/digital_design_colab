9ae6a5927a8fe174323a3a05bc51d43acd9b701d
Adam Shaeffer <49406062+adamshaeffer@users.noreply.github.com>
2023-05-31 14:21:52 -0600

    Update frq_sl.py

--- Exercises/sequential_logic/files/frq_sl.py
+++ Exercises/sequential_logic/files/frq_sl.py
@@ -86,16 +86,12 @@ Answers can be String or Int type. This does change the function used to create 
 The key is Q + Question Index. 
 """
 frquestions = {
-    "Q1": ["In KHZ:", 200, "If the Period is 5 Microseconds what is the Frequency?"],
-    "Q2": ["In MHZ:", 40, "If the Period is 25 ns what is the Frequency? "],
-    "Q3": ["In ns:", 0.1, "If the Frequency is 10GHZ what is the Period?"],
-    "Q4": ["In ms:", 40, "If the frequency is 25Hz what is the Period?"],
-    "Q5": ["In ns", 10, "If the frequency of the Clock is 100MHZ, what is the period?"],
-    "Q6": [
-        "Number of clock cycles:",
-        1000000,
-        "So, how many clock cycles in 1/10th of a second? ",
-    ],
+    "Q1": ["In KHZ:", 200, "If the period is 5 microseconds what is the frequency?"],
+    "Q2": ["In MHZ:", 40, "If the period is 25 ns what is the frequency? "],
+    "Q3": ["In ns:", "0.1", "If the frequency is 10 GHZ what is the period?"],
+    "Q4": ["In ms:", 40, "If the frequency is 25 Hz what is the period?"],
+    "Q5": ["In ns", 10, "If the frequency of the Clock is 100 MHZ, what is the period?"],
+    "Q6": ["Number of clock cycles:", 10000000, "Using the same clock frequency as the last question, how many clock cycles occur in 1/10th of a second?"],
 }
 
 
