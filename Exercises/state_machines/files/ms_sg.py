a5cd84500c400a678b364ca7f63381c95175cf2c
Garrett Smith <mrgarrettmichaelsmith@gmail.com>
2023-05-31 14:28:04 -0600

    update statemachines

--- Exercises/state_machines/files/ms_sg.py
+++ Exercises/state_machines/files/ms_sg.py
@@ -36,8 +36,8 @@ def check_answerms(self):
     """
     if self.current_answers == {}:
         return
-    for i in self.answer:
-        if i not in self.current_answers:
+    for i in self.current_answers:
+        if i not in self.answer:
             self.button_style = "Danger"
             self.description = "Wrong, Try Again!"
             return
@@ -58,11 +58,11 @@ ms_dict = {
         "C",
         "D",
         "E",
-        "BE",
-        "Which States are Complete and Full?",
+        "E",
+        "Which States are Complete?",
     ],
-    "2": ["A", "B", "C", "D", "E", "CD", "Which States are Incomplete?"],
-    "3": ["A", "B", "C", "D", "E", "AD", "Which States are Conflicting?"],
+    "2": ["A", "B", "C", "D", "E", "ABCD", "Which States are Incomplete?"],
+    "3": ["A", "B", "C", "D", "E", "ABCD", "Which States are Conflicting?"],
 }
 
 
