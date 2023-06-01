67398a53b08a5db1a6e9e62f82c89534dfa9dd8e
tr-rocks <83432874+tr-rocks@users.noreply.github.com>
2023-06-01 11:32:08 -0600

    Update frqRegisters.py

--- Exercises/registers/files/frqRegisters.py
+++ Exercises/registers/files/frqRegisters.py
@@ -91,10 +91,10 @@ frquestions = {"Q1": ["What is the value of Q at time 3?", "1", "Question 1"],
                "Q3": ["What is the value of Q at time 5?", "2", "Question 3"],
                "Q4": ["What is the value of Q at time 6?", "0", "Question 4"],
                "Q5": ["What is the value of Q at time 7?", "1", "Question 5"],
-               "Q6": ["What time interval does the first falling edge of Q occur?", "6-7", "Question 5"],
-               "Q7": ["What time interval does the first rising edge of Q' occur?", "6-7", "Question 5"],
-               "Q8": ["What time interval does the next rising edge of Q occur?", "7-8", "Question 5"],
-               "Q9": ["What time interval does the next falling edge of Q' occur?", "8-9", "Question 5"],
+               "Q6": ["What time interval does the first falling edge of Q occur?", "6-7", "Question 6"],
+               "Q7": ["What time interval does the first rising edge of Q' occur?", "6-7", "Question 7"],
+               "Q8": ["What time interval does the next rising edge of Q occur?", "7-8", "Question 8"],
+               "Q9": ["What time interval does the next falling edge of Q' occur?", "8-9", "Question 9"],
 }
 
 
