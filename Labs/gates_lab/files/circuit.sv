95af45a2b2ff29ec644957f5b24f42ff7fcbdeb4
Caleb Price <calebsp12@gmail.com>
2023-06-20 11:50:06 -0600

    1 test added to testbench

--- Labs/gates_lab/files/circuit.sv
+++ Labs/gates_lab/files/circuit.sv
@@ -1,6 +1,21 @@
 module circuit(
-
+   input wire logic A,
+   input wire logic B,
+   input wire logic C,
+   input wire logic D,
+   output logic Q
 );
+
+logic e, f, g, h, k, q;
+
+and(e, A, B); 
+or(f, B, C); 
+and(g,C,D); 
+not(h,g);
+and(k,f,h);
+or(q,k,e);
+not(Q,q); 
 
 
 endmodule
+
---
+++ Labs/gates_lab/files/tb_task1.cpp
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <iostream>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vtask1.h"
+
+
+void check(int expected_value, Vtask1 *dut){
+  int real_value = dut->led;
+  int sw = dut->sw;
+  int btnd = dut->btnd;
+  if(real_value != expected_value){
+    printf("ERROR: expected led = %d, instead led = %d while sw = %d and btnd = %d\n", expected_value, real_value, sw, btnd);
+  }
+  else{
+    printf("CORRECT: led = %d while sw = %d and btnd = %d\n", expected_value, sw, btnd);
+  }
+}
+
+void set_values(int A, int B, int C, int D, Vtask1 *dut){
+  dut->A = 0;
+  dut->B = 0; 
+  dut->C = 0; 
+  dut->D = 0; 
+  dut->eval();
+}
+
+int main(int argc, char **argv, char **env)
+{
+    Vtask1 *dut = new Vtask1;
+
+    Verilated::traceEverOn(true);
+    //Signals: btnr, sw
+    set_values(0, 0, dut);
+    check(1, dut);
+
+    delete dut;
+    exit(EXIT_SUCCESS);
+}
