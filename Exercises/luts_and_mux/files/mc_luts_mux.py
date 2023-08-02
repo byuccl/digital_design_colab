5512588bb57b4190fe67be01924b748dad5475e3
Caleb Price <calebsp12@gmail.com>
2023-08-02 10:23:06 -0600

    converted luts mux lab mc to use template

--- Exercises/luts_and_mux/files/mc_luts_mux.py
+++ Exercises/luts_and_mux/files/mc_luts_mux.py
@@ -1,47 +1,3 @@
-# @title Multiple Choice Creation
-from IPython.display import IFrame, display, HTML
-import ipywidgets as widgets
-from ipywidgets import GridspecLayout
-from ipywidgets import AppLayout, Button, Layout, jslink, IntText, IntSlider
-
-# function of creating button widget
-def create_expanded_button(description, button_style, width="400px"):
-    return Button(
-        description=description,
-        button_style=button_style,
-        layout=Layout(height="auto", width=width),
-    )
-
-
-# function of creating grid layout
-def create_grid(A, B, C, D):
-    grid = GridspecLayout(5, 3, width="470px")
-    grid[0, 0] = create_expanded_button(A, "primary")
-    grid[1, 0] = create_expanded_button(B, "primary")
-    grid[2, 0] = create_expanded_button(C, "primary")
-    grid[3, 0] = create_expanded_button(D, "primary")
-    grid[4, 0] = create_expanded_button("Submit", "Danger")
-    return grid
-
-
-def check_answermc4(self):
-    """
-    Used with .on_click to make sure the answer is correct
-        self: This is a button. We have added 2 new attributes to it
-        current_answer: This is the last clicked button and its description value stored as a str
-        answer: This is the correct answer from the mcdict
-
-    """
-    if self.current_answer == 0:
-        return
-    if self.answer[0] == self.current_answer[0]:
-        self.button_style = "Success"
-        self.description = "Correct!"
-    else:
-        self.button_style = "Danger"
-        self.description = "Wrong, Try Again!"
-
-
 """
 key: The question number
 pair: This should be a list. indexes 0-3 are the choices. 4 is the answer. 5 is the question
@@ -50,127 +6,70 @@ question: This is used in the header
 """
 mc_dict = {
     "1": [
-        "A. MUX",
+        ["A. MUX",
         "B. LUT",
         "C. Not Possible",
-        "D. A and B",
+        "D. A and B"],
         "A",
         "What should be used to implement an if/else statement?",
     ],
     "2": [
-        "A. MUX",
+        ["A. MUX",
         "B. LUT",
         "C. Not Possible",
-        "D. A and B",
+        "D. A and B"],
         "B",
         "What should primarily be used to implement the memory of a function?",
     ],
     "3": [
-        "A. 10111",
+        ["A. 10111",
         "B. 11101",
         "C. 101",
-        "D. Syntax Error",
+        "D. Syntax Error"],
         "A",
         "What is the Value of {A,B} where A = 3’b101 and B = 2’11?",
     ],
     "4": [
-        "A. 1010",
+        ["A. 1010",
         "B. 111000",
         "C. 101010",
-        "D. Syntax Error",
+        "D. Syntax Error"],
         "C",
         "What is the value of {3{2’b10}}?",
     ],
     "5": [
-        "A.logic input [3:0] x",
+        ["A.logic input [3:0] x",
         "B. logic [3:0] input x",
         "C. input logic x [3:0]",
-        "D. input logic [3:0] x",
+        "D. input logic [3:0] x"],
         "D",
         "Which signal is defined correctly?",
     ],
     "6": [
-        "A 1’b1",
+        ["A 1’b1",
         "B. 1’b0",
         "C. An Error will be thrown",
-        "D. Undefined",
+        "D. Undefined"],
         "C",
         "What is the value of x from the code above?",
     ],
     "7": [
-        "A. 1’b1",
+        ["A. 1’b1",
         "B. 1’b0",
         "C. 2’b11",
-        "D. An Error will be thrown",
+        "D. An Error will be thrown"],
         "A",
         "What is the value of x from the above code?",
     ],
     "8": [
-        "A. 4'b0001",
+        ["A. 4'b0001",
         "B. 4'b0011",
         "C. 4'b0111",
-        "D. 4'b1001",
+        "D. 4'b1001"],
         "D",
         "What is the value of x in the above code?",
     ],
 }
 
-
-def multiple_choice4(key, dictionary):
-    """
-    Creates a multiple choice widget that has 4 options.
-    key: This is the key to the dictionary. It will be a str of digits.
-    dictionary: the dictionary used is mc_dict. Which houses the multiple choice answers.
-    grid: The grid that has been set up and can be shown
-    header_button: sets the header_button that houses the question description
-    """
-    answers = dictionary[key]
-    question = answers[5]
-    A = answers[0]
-    B = answers[1]
-    C = answers[2]
-    D = answers[3]
-    grid = create_grid(A, B, C, D)
-    correct_answer = answers[4]
-    setattr(grid[4, 0], "answer", correct_answer)
-    current_answer = 0
-    setattr(grid[4, 0], "current_answer", 0)
-    grid[4, 0].button_style = "Warning"
-
-    def change_color(self):
-        current_btn = int(self.layout.grid_area[-1])
-        for i in range(0, 4):
-            grid[i, 0].button_style = "primary"
-        grid[current_btn - 1, 0].button_style = "info"
-        grid[4, 0].current_answer = self.description
-
-    for i in range(0, 4):
-        grid[i, 0].on_click(change_color)
-    grid[4, 0].on_click(check_answermc4)
-    header_button = create_expanded_button(question, "info", "465px")
-    return grid, header_button
-
-
-def create_mc_grids():
-    mc_grids = {}
-    i = 1
-    for key in mc_dict:
-        mc_grids[key] = multiple_choice4(key, mc_dict)
-        i += 1
-    return mc_grids
-
-
-mc_grids = create_mc_grids()
-
-
-def print_mc_grid(question_number):
-    key = str(question_number)
-    # Create the head tab
-    widgets.VBox(
-        [
-            widgets.HTML(value="<span id='something'></span>"),
-            AppLayout(header=mc_grids[key][1], footer=None),
-            mc_grids[key][0],
-        ]
-    )
-    display(widgets.VBox([mc_grids[key][1], mc_grids[key][0]]))
+def get_data():
+  return mc_dict
