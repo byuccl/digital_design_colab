18050ba8b928eda2c439fb7a6eff63a1716ef161
Colton Yates <coltony@byu.edu>
2024-03-12 18:17:13 -0600

    fix for tt_template in boolean_algebra lesson

---
+++ Exercises/boolean_algebra/files/tt_boolean_key.py
@@ -0,0 +1,18 @@
+"""
+Holds the values for the truth tables.
+Structure:
+------------
+    key: Q + question number.
+List 
+    [0] : The number of inputs
+    [1] : The correct answer, MSB is the top row
+"""
+tt = {
+    "Q1": [2, "A' * B + A' + B'", "1110"],
+    "Q2": [3, "ABC + B'C + A'C'", "11100101"],
+    "Q3": [3, "A'B'C + AC + BC", "01010101"],
+    "Q4": [4, "ABCD' + A'BCD' + BC'D + B +  BC'D' + C'D", "0100111101001111"],
+}
+
+def get_tt_data():
+    return tt
