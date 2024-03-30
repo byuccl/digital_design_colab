4c71f95d606182fc5e86e0d3bf0488c6328fe2f0
jamilkhalaf <jamil04k@byu.edu>
2024-03-29 23:18:45 -0600

    most

---
+++ Exercises/luts_and_mux/mc_luts_mux.py
@@ -0,0 +1,73 @@
+"""
+key: The question number
+pair: [List of all the options, can be any length], Answer(This must be the same str value as the choice), Question
+"""
+mc_dict = {
+    "1": [
+        ["A. MUX",
+        "B. LUT",
+        "C. Not Possible",
+        "D. A and B"],
+        "A",
+        "What should be used to implement an if/else statement?",
+    ],
+    "2": [
+        ["A. MUX",
+        "B. LUT",
+        "C. Not Possible",
+        "D. A and B"],
+        "B",
+        "What should primarily be used to implement the memory of a function?",
+    ],
+    "3": [
+        ["A. 10111",
+        "B. 11101",
+        "C. 101",
+        "D. Syntax Error"],
+        "A",
+        "What is the Value of {A,B} where A = 3’b101 and B = 2’11?",
+    ],
+    "4": [
+        ["A. 1010",
+        "B. 111000",
+        "C. 101010",
+        "D. Syntax Error"],
+        "C",
+        "What is the value of {3{2’b10}}?",
+    ],
+    "5": [
+        ["A.logic input [3:0] x",
+        "B. logic [3:0] input x",
+        "C. input logic x [3:0]",
+        "D. input logic [3:0] x"],
+        "D",
+        "Which signal is defined correctly?",
+    ],
+    "6": [
+        ["A 1’b1",
+        "B. 1’b0",
+        "C. An Error will be thrown",
+        "D. Undefined"],
+        "C",
+        "What is the value of x from the code above?",
+    ],
+    "7": [
+        ["A. 1’b1",
+        "B. 1’b0",
+        "C. 2’b11",
+        "D. An Error will be thrown"],
+        "A",
+        "What is the value of x from the above code?",
+    ],
+    "8": [
+        ["A. 4'b0001",
+        "B. 4'b0011",
+        "C. 4'b0111",
+        "D. 4'b1001"],
+        "D",
+        "What is the value of x in the above code?",
+    ],
+}
+
+def get_data():
+  return mc_dict
