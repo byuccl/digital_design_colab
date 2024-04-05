c9a62ebcfe8ca3568de6edc37ed8f7634f1ae900
Colton Yates <coltony@byu.edu>
2024-04-05 14:43:14 -0600

    updated input variable

--- Exercises/luts_and_mux/files/tt_luts_mux.py
+++ Exercises/luts_and_mux/files/tt_luts_mux.py
@@ -8,7 +8,7 @@ List
     [1] : The correct answer, MSB is the top row
 """
 tt = {
-    "Q1": [3 , "2:1 MUX Table, with A, B, S inputs", "00011011"],
+    "Q1": [5, "2:1 MUX Table, with A, B, S inputs", "00011011"],
 }
 
 def get_tt_data():
